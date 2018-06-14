462fb2597028cc7357c570640def3d85adcdcbce
Pablo Madrigal <pjmq2@hotmail.com>
2018-06-14 14:19:15 -0600

    Guarda las intrucciones en memoria principal y crea los contextos

--- Module/src/MainThread.java
+++ Module/src/MainThread.java
@@ -2,27 +2,54 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainThread
 {
     public static void main(String[] args)
     {
         MainThread mainThread = new MainThread();
-        mainThread.leerHilillos("C:\\Users\\pjmq2\\Documents\\MEGAsync\\UCR\\Informatica\\CI-1323 Arquitectura de Computadores\\Proyecto\\AoKuang\\Module\\Hilillos\\0.txt");
+        int posicion=0;
+        posicion=mainThread.leerHilillos("C:\\Users\\pjmq2\\Documents\\MEGAsync\\UCR\\Informatica\\CI-1323 Arquitectura de Computadores\\Proyecto\\AoKuang\\Module\\Hilillos\\0.txt",posicion);
+        posicion=mainThread.leerHilillos("C:\\Users\\pjmq2\\Documents\\MEGAsync\\UCR\\Informatica\\CI-1323 Arquitectura de Computadores\\Proyecto\\AoKuang\\Module\\Hilillos\\1.txt",posicion);
+        posicion=mainThread.leerHilillos("C:\\Users\\pjmq2\\Documents\\MEGAsync\\UCR\\Informatica\\CI-1323 Arquitectura de Computadores\\Proyecto\\AoKuang\\Module\\Hilillos\\2.txt",posicion);
+        posicion=mainThread.leerHilillos("C:\\Users\\pjmq2\\Documents\\MEGAsync\\UCR\\Informatica\\CI-1323 Arquitectura de Computadores\\Proyecto\\AoKuang\\Module\\Hilillos\\3.txt",posicion);
+        posicion=mainThread.leerHilillos("C:\\Users\\pjmq2\\Documents\\MEGAsync\\UCR\\Informatica\\CI-1323 Arquitectura de Computadores\\Proyecto\\AoKuang\\Module\\Hilillos\\4.txt",posicion);
+        posicion=mainThread.leerHilillos("C:\\Users\\pjmq2\\Documents\\MEGAsync\\UCR\\Informatica\\CI-1323 Arquitectura de Computadores\\Proyecto\\AoKuang\\Module\\Hilillos\\5.txt",posicion);
+
     }
+
+    private int[] memoriaPrincipalDatos;
+    private int[] memoriaPrincipalInstrucciones;
+    private List<Contexto> contextoList;
 
     public MainThread() {
+        memoriaPrincipalDatos = new int[96];
+        memoriaPrincipalInstrucciones = new int[640];
+
+        for (int i = 0; i < memoriaPrincipalInstrucciones.length; i++)
+            memoriaPrincipalInstrucciones[i] = 1;
+        for (int i = 0; i < memoriaPrincipalDatos.length; i++)
+            memoriaPrincipalDatos[i] = 1;
+        contextoList = new ArrayList<Contexto>();
     }
 
-    private void leerHilillos (String ruta){
+    private int leerHilillos (String ruta, int posicionMemInstr){
+        Contexto contexto = new Contexto();
+        contexto.setPC(posicionMemInstr);
+        contexto.setID(Integer.parseInt(""+ruta.charAt(ruta.length() - 5)));
         try {
             BufferedReader br = new BufferedReader(new FileReader(ruta));
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
             while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
+                String[] data = line.split(" ");
+                for(int i = 0; i < data.length ; i++){
+                    memoriaPrincipalInstrucciones[posicionMemInstr]=Integer.parseInt(data[i]);
+                    posicionMemInstr++;
+                }
                 line = br.readLine();
             }
             String everything = sb.toString();
@@ -33,5 +60,7 @@ public class MainThread
         } catch (IOException e) {
             e.printStackTrace();
         }
+        contextoList.add(contexto);
+        return posicionMemInstr;
     }
 }
