c55e457606f8a1ff780f12afa571eb18d5dd0ca3
B55519 <krmanxv@gmail.com>
2018-06-14 14:21:19 -0600

    Clase Nucleo

---
+++ Module/src/Nucleo.java
@@ -0,0 +1,17 @@
+public class Nucleo
+{
+    private int numNucleo;
+
+    public Nucleo(){}
+
+    public Nucleo(int numero)
+    {
+        numNucleo = numero;
+    }
+
+    public Contexto procesar(Contexto contexto)
+    {
+        return contexto;
+    }
+}
+
