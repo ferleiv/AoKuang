6f6958f1e6b77bf6c24dd242e709d78e0ec8d73a
Karman <krmanxv@gmail.com>
2018-07-01 17:14:09 -0600

    Ultimos comentarios y un par de pequeños ajustes

--- Module/src/Contexto.java
+++ Module/src/Contexto.java
@@ -3,7 +3,7 @@ import java.util.Vector;
 
 public class Contexto
 {   //Clase para la abstranccion del contexto de cada hilillo
-    private int ID, PC, index;
+    private int ID, PC; //Identificador de hilillo, Program Counter
     private int[] registros; //Vector de los registros
     private int Cache; /* 0 = Nucleo 0
                           1 = Nucleo 1 */
@@ -12,72 +12,76 @@ public class Contexto
                            1 = Fallo Datos,
                            2 = Fallo Instrucciones */
 
+    //Constructor vacio que inicializa de forma muy basica
     public Contexto()
     {
         estado = 0;
         registros = new int[32];
     }
 
-    public Contexto(int nID, int nPC, int idx)
-    {
-        ID = nID;
-        PC = nPC;
-        index = idx;
-        estado = 0;
-        registros = new int[32];
-    }
-
+    //Getter del identificador de hilillo
     public int getID()
     {
         return ID;
     }
 
+    //Setter del identificador del hilillo
     public void setID(int nID)
     {
         ID = nID;
     }
 
+    //Getter del Program Counter
     public int getPC()
     {
         return PC;
     }
 
+    //Setter del Program Counter
     public void setPC(int nPC)
     {
         PC = nPC;
     }
 
+    //Getter de los registros
     public int[] getRegistros()
     {
         return registros;
     }
 
+    //Setter de los registros
     public void setRegistros(int[] nRegs)
     {
         registros = nRegs;
     }
 
+    //Getter del numero de cache en el que esta
     public int getCache()
     {
         return Cache;
     }
 
+    //Setter de la cache en la que se encuentra
     public void setCache(int nCache)
     {
         Cache = nCache;
     }
 
+    //Getter del estado en el que se encuentra
     public int getEstado()
     {
         return estado;
     }
 
+    //Setter del estado en el que esta
     public void setEstado(int nEstado)
     {
         estado = nEstado;
     }
 
-    public void setRegistro(int posicion, int valor){registros[posicion]=valor;}
-
+    //Getter de un registro en especifico
     public int getRegistro(int posicion){return registros[posicion];}
+
+    //Setter de un registro especifico
+    public void setRegistro(int posicion, int valor){registros[posicion]=valor;}
 }
